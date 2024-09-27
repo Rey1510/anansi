2eb19fda6c42078961c08125cff0665662e50ba5
Reynard A. <reynardaurele1510@gmail.com>
2024-09-27 15:05:02 +0700

    Story2

--- exercise/day-1/HelloWorld/src/Main.kt
+++ exercise/day-1/HelloWorld/src/Main.kt
@@ -1,18 +1,17 @@
 fun main() {
-    var grade: Int
-    grade = -1
-    println("Your grade is ")
-    if (grade > 100 && grade < 0) {
-        println("Masukkin yang bener WOI!")
-    }else if (grade in 90..100) {
-        println("A")
-    }else if (grade in 80..89){
-        println("B")
-    }else if (grade in 70..79) {
-        println("C")
-    }else if (grade in 60..69) {
-        println("D")
-    }else {
-        println("F")
+    var salary : Int;
+    var timeWorked : Int;
+
+    salary = 100000
+    timeWorked = 3
+    if (timeWorked < 0){
+        println("Masukkin yang bener bang!")
+    } else {
+        val total = when {
+            timeWorked in 2..5 -> salary * 0.1
+            timeWorked > 5 -> salary * 0.2
+            else -> 0
+        }
+        println("Bonus: $total")
     }
 }
