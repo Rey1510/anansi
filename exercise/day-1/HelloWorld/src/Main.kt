d981c2668125685a160bf8830b90496f13148c2e
Reynard A. <reynardaurele1510@gmail.com>
2024-09-27 14:44:18 +0700

    Story 1

--- exercise/day-1/HelloWorld/src/Main.kt
+++ exercise/day-1/HelloWorld/src/Main.kt
@@ -1,3 +1,18 @@
 fun main() {
-    println("Hello World!")
+    var grade: Int
+    grade = 101
+    println("Your grade is ")
+    if (grade > 100) {
+        println("Masukkin yang bener WOI!")
+    }else if (grade in 90..100) {
+        println("A")
+    }else if (grade in 80..89){
+        println("B")
+    }else if (grade in 70..79) {
+        println("C")
+    }else if (grade in 60..69) {
+        println("D")
+    }else {
+        println("F")
+    }
 }
