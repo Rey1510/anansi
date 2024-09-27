c664f95aabb582372be80453c7443df4ffaee926
Reynard A. <reynardaurele1510@gmail.com>
2024-09-27 16:27:22 +0700

    Story 5

--- exercise/day-1/HelloWorld/src/Main.kt
+++ exercise/day-1/HelloWorld/src/Main.kt
@@ -1,14 +1,19 @@
 fun main() {
-    var usage = 201.0
-    var total: Double = 0.0
+    val secretNumber = 7
+    val guesses = arrayOf(5, 3, 7, 0)
+    var index = 0
 
-    if (usage <= 100 && usage >= 0){
-        total = usage
-    } else if (usage > 100 && usage <=200){
-        total = 100 + ((usage-100)*1.5)
-    } else if(usage > 200) {
-        total = 250 + ((usage-200)*2)
+    while (index < guesses.size) {
+        val guess = guesses[index]
+        index++
+        if (guess == 0) {
+            println("Game berhenti, guess 0.")
+            break
+        } else if (guess == secretNumber) {
+            println("Selamat! Angka benar: $secretNumber")
+            break
+        } else {
+            println("Guess $guess salah. Coba lagi!")
+        }
     }
-
-    println("Total $total")
 }
